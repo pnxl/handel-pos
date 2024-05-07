1454af0c789d096d2171549baa9c75179df521d7
Jason <development@pnxl.dev>
2024-05-07 10:33:35 +0700

    styles: add max-h-screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="min-h-screen min-w-full bg-gray-100 text-gray-800">
+  <div
+    class="min-h-screen min-w-full max-h-screen flex flex-row bg-gray-100 text-gray-800"
+  >
+    <div class="bg-gray-200 p-4"></div>
     <slot />
   </div>
 </template>
