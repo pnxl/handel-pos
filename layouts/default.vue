9d5ff0172fad917157d593dec599e9cc4f68834c
Jason <development@pnxl.dev>
2024-05-07 10:29:17 +0700

    styles: remove dark variants

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,5 @@
 <template>
-  <div
-    class="
-      min-h-screen min-w-full
-      bg-gray-100
-      text-gray-800
-      dark:bg-gray-900
-      dark:text-gray-200
-    "
-  >
+  <div class="min-h-screen min-w-full bg-gray-100 text-gray-800">
     <slot />
   </div>
 </template>
