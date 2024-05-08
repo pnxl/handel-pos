eddd0e753fdb419ba95681037160f5551a971cac
Jason <development@pnxl.dev>
2024-05-08 12:28:33 +0700

    chore: add placeholder history button and increase padding between buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     class="min-h-screen min-w-screen max-h-screen max-w-screen flex flex-row bg-gray-100 text-gray-800"
   >
     <div class="bg-gray-200 p-4 flex flex-col justify-between gap-y-4">
-      <div class="flex flex-col gap-y-4">
+      <div class="flex flex-col gap-y-6">
         <nuxt-link to="/" class="flex flex-col w-full group">
           <div
             :class="
@@ -31,32 +31,59 @@
             Cashier
           </p>
         </nuxt-link>
-        <nuxt-link to="/database" class="flex flex-col w-full group">
+        <nuxt-link to="/menu" class="flex flex-col w-full group">
           <div
             :class="
-              (route.path === '/database' ? 'bg-indigo-300' : '') +
+              (route.path === '/menu' ? 'bg-indigo-300' : '') +
               ' rounded-full bg-opacity-50 px-5 py-1 group group-hover:bg-indigo-200 group-hover:bg-opacity-50'
             "
           >
             <Icon
-              name="mdi:database"
+              name="mdi:list-box"
               class="text-2xl text-indigo-900 group-hover:scale-110 group-hover:text-indigo-800"
-              v-if="route.path === '/database'"
+              v-if="route.path === '/menu'"
             />
             <Icon
-              name="mdi:database-outline"
+              name="mdi:list-box-outline"
               class="text-2xl text-gray-800 group-hover:scale-110 group-hover:text-indigo-700"
               v-else
             />
           </div>
           <p
             :class="
-              (route.path === '/database'
+              (route.path === '/menu' ? 'text-indigo-900' : 'text-gray-700') +
+              ' text-center font-medium'
+            "
+          >
+            Menu
+          </p>
+        </nuxt-link>
+        <nuxt-link to="/history" class="flex flex-col w-full group">
+          <div
+            :class="
+              (route.path === '/history' ? 'bg-indigo-300' : '') +
+              ' rounded-full bg-opacity-50 px-5 py-1 group group-hover:bg-indigo-200 group-hover:bg-opacity-50'
+            "
+          >
+            <Icon
+              name="mdi:history"
+              class="text-2xl text-indigo-900 group-hover:scale-110 group-hover:text-indigo-800"
+              v-if="route.path === '/history'"
+            />
+            <Icon
+              name="mdi:history"
+              class="text-2xl text-gray-800 group-hover:scale-110 group-hover:text-indigo-700"
+              v-else
+            />
+          </div>
+          <p
+            :class="
+              (route.path === '/history'
                 ? 'text-indigo-900'
                 : 'text-gray-700') + ' text-center font-medium'
             "
           >
-            Database
+            History
           </p>
         </nuxt-link>
       </div>
