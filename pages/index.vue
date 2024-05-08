ddb044604dcdd288490cd3473f0447b59c9bfd9f
Jason <development@pnxl.dev>
2024-05-08 11:25:11 +0700

    feat: current order calculator

--- pages/index.vue
+++ pages/index.vue
@@ -8,36 +8,91 @@
     </div>
     <div class="flex flex-row gap-x-2 max-h-[87%]">
       <div
-        class="overflow-y-auto p-6 flex flex-col gap-y-6 bg-gray-50 rounded-l-xl"
+        class="overflow-y-auto p-6 flex flex-col gap-y-6 w-full bg-gray-50 rounded-l-xl"
       >
         <div class="flex flex-col gap-y-4">
           <h1 class="font-bold text-xl">Smoothies</h1>
-          <div class="flex flex-wrap gap-x-4 gap-y-4">
-            <div
-              class="rounded-xl border-gray-300 border-2 w-40 flex-auto hover:border-indigo-400"
+          <div
+            class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-4 gap-y-4"
+          >
+            <button
+              class="rounded-xl border-gray-300 text-left border-2 w-40 flex-auto hover:border-indigo-400"
               v-for="item in menuItems"
               :key="item.id"
+              @click="
+                currentOrder.push({
+                  id: orderId++,
+                  name: item.name,
+                  image: item.image,
+                  price: item.price,
+                });
+                console.log(currentOrder);
+              "
             >
-              <img
-                :src="`${config.public.databaseUrl}/storage/v1/object/public/menu/${item.image}`"
-                class="rounded-t-xl h-40 w-full object-cover aspect-square"
-              />
-              <div class="p-4 flex flex-col gap-y-2">
+              <div class="p-3 flex flex-col gap-y-2">
+                <img
+                  :src="`${config.public.databaseUrl}/storage/v1/object/public/menu/${item.image}`"
+                  class="rounded-xl w-full object-cover aspect-square"
+                />
                 <p class="text-lg font-semibold">{{ item.name }}</p>
                 <p class="opacity-75 mb-4">
                   {{ String(item.ingredients.join(", ")) }}
                 </p>
                 <p class="">Rp {{ item.price.toLocaleString("id-id") }}</p>
               </div>
-            </div>
+            </button>
           </div>
         </div>
       </div>
       <div
-        class="h-full overflow-y-auto p-6 flex flex-col gap-y-6 bg-gray-50 rounded-r-xl w-2/5"
+        class="h-full p-6 flex flex-col gap-y-6 bg-gray-50 rounded-r-xl w-1/2"
       >
-        <h1 class="font-bold text-xl">Smoothies</h1>
-        <div class="flex flex-col gap-y-4"></div>
+        <h1 class="font-bold text-xl">Current Order</h1>
+        <div class="flex flex-col gap-y-4 overflow-y-auto">
+          <div v-for="item in currentOrder" :key="item.id">
+            <div class="flex flex-row justify-between gap-x-4 my-auto">
+              <div class="flex flex-row gap-x-2">
+                <img
+                  :src="`${config.public.databaseUrl}/storage/v1/object/public/menu/${item.image}`"
+                  class="rounded-xl object-cover aspect-square w-16 h-16"
+                />
+                <div class="my-auto">
+                  <p class="text-lg font-semibold">{{ item.name }}</p>
+                  <p class="">Rp {{ item.price.toLocaleString("id-id") }}</p>
+                </div>
+              </div>
+              <button
+                class="group"
+                @click="
+                  currentOrder.splice(
+                    currentOrder.findIndex((i) => i.id === item.id),
+                    1
+                  )
+                "
+              >
+                <Icon
+                  name="mdi:close-circle"
+                  class="text-2xl mx-auto my-auto text-red-800 group-hover:text-red-400"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="z-10 w-full bg-gray-50 flex flex-col gap-y-4">
+          <hr class="border-gray-300 rounded-full" />
+          <div class="flex flex-row justify-between">
+            <p class="opacity-75 my-auto">{{ currentOrder.length }} items</p>
+            <p class="font-medium text-lg my-auto">
+              Rp
+              {{
+                (totalPrice = currentOrder.reduce(
+                  (acc, item) => acc + item.price,
+                  0
+                )).toLocaleString("id-id")
+              }}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -55,6 +110,9 @@ const supabase = createClient(
 
 const quote = ref("");
 const menuItems = ref([]);
+const orderId = ref(1);
+const currentOrder = reactive([]);
+const totalPrice = reactive([]);
 
 async function getResults() {
   const quotes = (await supabase.from("quotes").select()).data;
