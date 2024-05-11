6def23537773eba9d9cd67d1e27c9572afae7e31
Jason <development@pnxl.dev>
2024-05-11 22:42:17 +0700

    feat: add custom template for other pages

---
+++ layouts/custom.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="min-h-screen min-w-screen max-h-screen max-w-screen flex flex-row bg-gray-100 text-gray-800"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const config = useRuntimeConfig();
+</script>
